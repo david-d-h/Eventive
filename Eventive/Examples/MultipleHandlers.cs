6cf6926d1e255d127bdc8511174264637c89c8fe
pixelafk <=>
2023-01-27 11:08:55 +0100

    Add new example

---
+++ Eventive/Examples/MultipleHandlers.cs
@@ -0,0 +1,55 @@
+﻿using Eventive.Models;
+
+// ReSharper disable once CheckNamespace
+namespace Eventive.Examples.MultipleHandlers;
+
+internal class SomeEvent : Event
+{
+    //
+}
+
+internal class AnotherEvent : Event
+{
+    //
+}
+
+internal class SomeListener : Listener,
+    IHandles<SomeEvent>,
+    IHandles<AnotherEvent>
+{
+    public void Handle(SomeEvent @event)
+    {
+        Console.WriteLine("Handling SomeEvent");
+    }
+
+    public void Handle(AnotherEvent @event)
+    {
+        Console.WriteLine("Handling AnotherEvent");
+    }
+}
+
+internal class EventService : EventServiceProvider
+{
+    private protected override Dictionary<Type, Type[]> Listen { get; } = new()
+    {
+        [Event<SomeEvent>()] = new[]
+        {
+            Listener<SomeListener>()
+        },
+        
+        [Event<AnotherEvent>()] = new[]
+        {
+            Listener<SomeListener>()
+        }
+    };
+}
+
+public class MultipleHandlers
+{
+    public static void Main()
+    {
+        var eventService = new EventService();
+        eventService.InvokeEvent(new SomeEvent());
+        eventService.InvokeEvent(new AnotherEvent());
+    }
+}
